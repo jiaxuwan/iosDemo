2260db4e3c1c02fa45636ddc346b2cd98304c0b2
wanjiaxu <wanjiaxu00@live.com>
2022-02-13 23:16:36 +0800

    creat APP

---
+++ stayHungry/stayHungry/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  stayHungry
+//
+//  Created by jiaxu on 2022/2/13.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    let window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
